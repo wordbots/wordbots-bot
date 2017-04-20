837c2d0fac39958e7ad7da9afa0393c15cd20899
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-04-19 21:52:56 -0700

    New way of sending the image, since embeds stopped working for some reason

--- bot.rb
+++ bot.rb
@@ -1,19 +1,26 @@
 require 'discordrb'
 require 'json'
 require 'erb'
+require 'active_support/core_ext/hash'
 
 include ERB::Util
 
 bot = Discordrb::Bot.new(token: ENV['DISCORD_API_TOKEN'], client_id: ENV['DISCORD_APP_ID'])
 
-bot.message(start_with: '[{"id":"') do |event|
+bot.message(start_with: ['[{"id":"', '{"id":"']) do |event|
   cards = JSON.parse(event.message.content)
+  cards = [cards] if cards.is_a?(Hash)
+
   cards.each do |card|
-    url = "http://app.wordbots.io/api/card.png?card=#{url_encode(JSON.generate(card))}"
-    event.channel.send_embed do |embed|
-      embed.title = card['name']
-      embed.image = Discordrb::Webhooks::EmbedImage.new(url: url)
-    end
+    card.slice!('name', 'type', 'cost', 'stats', 'text')
+    name, text = card['name'], card['text']
+    puts card
+
+    card['name'] = url_encode(card['name'])
+    card['text'] = url_encode(card['text'])
+    image_url = "http://app.wordbots.io/api/card.png?card=#{JSON.generate(card)}"
+
+    event.respond "**#{name}**\n#{text}\n#{image_url}"
   end
 end
 
